998da6969f2361ba091ddde6f9692b03989ef9b4
gueldir@gmail.com <gueldir@gmail.com@6345b76b-94ec-9e84-c4df-f5c450b954ad>
2014-11-16 15:55:13 +0000

    Ajout des signaux, de l'IHM (vue), COntroler, et Network (modele)

---
+++ ChatSystem/src/Signals/Goodbye.java
@@ -0,0 +1,21 @@
+package signals;
+
+import java.io.Serializable;
+
+public class Goodbye implements Serializable {
+	private String username;
+
+	public Goodbye(String username) {
+		super();
+		this.username = username;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+}
---
+++ ChatSystem/src/Signals/TextMessage.java
@@ -0,0 +1,44 @@
+package signals;
+
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.util.ArrayList;
+
+public class TextMessage implements Serializable {
+	private String message;
+	private String from;
+	private ArrayList<String> to;
+	
+	public TextMessage(String message, String from, ArrayList<String> to) {
+		super();
+		this.message = message;
+		this.from = from;
+		this.to = to;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public ArrayList<String> getTo() {
+		return to;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public void setTo(ArrayList<String> to) {
+		this.to = to;
+	}
+	
+	
+}
