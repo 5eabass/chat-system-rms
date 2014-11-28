3356c8bc2c9457d68d13985ebb70bed0982c067c
melina.manolias@gmail.com <melina.manolias@gmail.com@6345b76b-94ec-9e84-c4df-f5c450b954ad>
2014-11-28 22:58:54 +0000

    ajout de TCP

---
+++ trunk/ChatSystem/src/chatsystem/Test.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatsystem;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Test {
+
+    public Test() {
+    }
+
+    public static void main(String[] args) {
+        try {
+            TCPserver server = new TCPserver(1234);
+            TCPsender sender = new TCPsender(InetAddress.getLocalHost(), 1234);
+            sender.openClient();
+            server.start();
+        } catch (IOException ex) {
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
