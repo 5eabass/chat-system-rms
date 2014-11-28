dd07f802ee28755eaeb39481e88d7328dfa081e8
melina.manolias@gmail.com <melina.manolias@gmail.com@6345b76b-94ec-9e84-c4df-f5c450b954ad>
2014-11-28 22:58:54 +0000

    ajout de TCP

--- ChatSystem/src/chatsystem/TCPsender.java
+++ ChatSystem/src/chatsystem/TCPsender.java
@@ -1,13 +1,20 @@
 package chatsystem;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class TCPsender extends Thread {
 
+    final int TAILLE_MAX = 5; // Taille max de fichier de 5 Mo 
     private Socket s0;
     private InetAddress addrIp;
     private int dPort;
@@ -21,7 +28,11 @@ class TCPsender extends Thread {
         openClient();
     }
 
-    private void openClient() {
+    TCPsender(double d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void openClient() {
 
         try {
             s0 = new Socket(addrIp, dPort);
@@ -29,17 +40,51 @@ class TCPsender extends Thread {
             Logger.getLogger(TCPsender.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void run() {
-        while (true) {            
-            try {
-                sleep(3000);
-                System.out.println(">>> Waiting for some file to send...");
-            } catch (InterruptedException ex) {
-                Logger.getLogger(TCPsender.class.getName()).log(Level.SEVERE, null, ex);
+
+    public void sendFichier(File fichier) {
+        try {
+            DataInputStream input = new DataInputStream(s0.getInputStream());
+            DataOutputStream output = new DataOutputStream(s0.getOutputStream());
+            System.out.println(">>> Waiting for some file to send...");
+            FileReader reader;
+            reader = new FileReader("chanson.txt");
+            // On envoie la taille du fichier
+            if (fichier.length() > TAILLE_MAX) {
+                System.out.println("Le fichier que vous essayez d'envoyer dépasse la taille requise");
+            } else {
+                System.out.println("Length" + fichier.length());
+
+                // output.writeInt((int) fichier.length());
+                
+                System.out.println("Writing.......");
+                output.writeUTF(reader.toString());
+               // output.writeBytes(fichier.toString());
+
+                //Step 1 read length
+                int nb = input.readInt();
+                byte[] digit = new byte[nb];
+                //Step 2 read byte
+                for (int i = 0; i < nb; i++) {
+                    digit[i] = input.readByte();
+                }
+
+                String st = new String(digit);
+                System.out.println("Received: " + st);
             }
-            
+        } catch (UnknownHostException e) {
+            System.out.println("Sock:" + e.getMessage());
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            if (s0 != null) {
+                try {
+                    s0.close();
+                } catch (IOException e) {/*close failed*/
+
+                }
+            }
         }
     }
-
 }
--- ChatSystem/src/chatsystem/TCPserver.java
+++ ChatSystem/src/chatsystem/TCPserver.java
@@ -1,8 +1,43 @@
 package chatsystem;
 
-class TCPserver {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import signals.Signal;
 
-    public TCPserver() {
+public class TCPserver extends Thread {
+
+    private ServerSocket s0;
+    private int dPort;
+
+    public TCPserver(int p) {
+
+        try {
+            this.s0 = new ServerSocket(p);
+            this.dPort = p;
+        } catch (IOException ex) {
+            Logger.getLogger(TCPserver.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
+    public void run() {
+        try {
+            System.out.println("DEBUG *** TCPserver : run ***");
+            // reception des paquets
+            while (true) {
+
+                Socket client = s0.accept();
+                Connection c = new Connection(client);
+                System.out.println("DEBUG *** TCPserver : listening connection ***");
+                // ChatSystem.getNetwork().signalProcess(Signal.fromByteArray(buffer));
+            }
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
 }
