d8c7de0fade85c319f31fd0c3da7c92f060a3b3b
gueldir@gmail.com <gueldir@gmail.com@6345b76b-94ec-9e84-c4df-f5c450b954ad>
2014-12-09 16:08:32 +0000

    blabla5

--- trunk/ChatSystem/src/network/TCPserver.java
+++ trunk/ChatSystem/src/network/TCPserver.java
@@ -9,12 +9,13 @@ public class TCPserver extends Thread {
     private ServerSocket ss;
     private Socket s0;
     private Vector<TCPreceiver> rList;
-    private int port, max_connexion;
+    private int port, max_connexion,size;
     private String fileName;
 
-    public TCPserver(String fileName, int port) {
+    public TCPserver(String fileName, int size, int port) {
         this.rList = new Vector<TCPreceiver>();
         this.port = port;
+        this.size = size;
         this.fileName = fileName;
         this.max_connexion = 1;
     }
@@ -27,7 +28,7 @@ public class TCPserver extends Thread {
                 // Acceptation d'un flux d'entrée socket
                 s0 = ss.accept();
                 System.out.println("Connexion detected ***************");
-                rList.add(new TCPreceiver(s0, fileName));
+                rList.add(new TCPreceiver(s0, fileName, size));
                 rList.lastElement().start();
                 if (!rList.lastElement().isAlive()) {
                     ss.close();
